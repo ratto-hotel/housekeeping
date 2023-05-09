54c1e9049824b906e61db1d79c3b83abd994cf79
Jean Nunes <jeanrnunes@gmail.com>
2023-05-09 10:03:59 -0300

    fixed page title

--- resources/views/manage-badge/index.blade.php
+++ resources/views/manage-badge/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout.app>
-    @push('title', 'Bans')
+    @push('title', 'Badges')
 
     <div class="container-fluid">
         <h3 class="text-dark mb-4">{{ __('Manage badges') }}</h3>
 
-        <x-messages.flash-messages />
+        <x-messages.flash-messages/>
 
         <div class='card mb-2'>
             <div class='card-body'>
@@ -14,7 +14,7 @@
 
                     <div class="input-group mb-3">
                         <input name="image" id="image" type="file"
-                            class="form-control @error('file') is-invalid @enderror">
+                               class="form-control @error('file') is-invalid @enderror">
                         <div class="input-group-append">
                             <x-elements.primary-button tooltip-text="{{ __('Create badge') }}">
                                 <i class="fas fa-send"></i>
@@ -31,57 +31,58 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive table mt-2" id="dataTable" role="grid"
-                    aria-describedby="dataTable_info">
+                     aria-describedby="dataTable_info">
                     <table class="table my-0" id="dataTable">
                         <thead>
-                            <tr>
-                                <th>{{ __('Username') }}</th>
-                                <th>{{ __('Badge') }}</th>
-                                <th>{{ __('Status') }}</th>
-                                <th></th>
-                            </tr>
+                        <tr>
+                            <th>{{ __('Username') }}</th>
+                            <th>{{ __('Badge') }}</th>
+                            <th>{{ __('Status') }}</th>
+                            <th></th>
+                        </tr>
                         </thead>
                         <tbody>
-                            @foreach ($badges as $badge)
-                                <tr>
-                                    <td class="d-flex overflow-hidden" style="height: 50px;">
-                                        <img class="avatar"
-                                            src="{{ setting('avatar_imager') }}{{ $badge->user?->look }}&direction=2&headonly=1&head_direction=2&gesture=sml"
-                                            alt="">
-                                        {{ $badge->user?->username }}
-                                    </td>
-                                    <td>
-                                        <img src="https://www.rattohotel.com/swf/c_images/Album1584/{{ $badge->badge_imaging }}"
-                                            height="32" width="32" />
-                                        {{ $badge->badge_imaging }}
-                                    </td>
-                                    <td>{{ __('badge_status.' . $badge->status) }}</td>
-                                    <td>
-                                        @if ($badge->status === 'open')
-                                            <form class="ml-2" action="{{ route('manage-badges.accept', $badge) }}"
-                                                method="POST" onSubmit='confirmDelete("confirmBadgeAccept");'
-                                                onSubmit="return confirm('{{ __('Are you sure you want to accept this badge?') }}');">
-                                                @method('PUT')
-                                                @csrf
-
-                                                <x-elements.success-button tooltip-text="{{ __('Approve badge') }}">
-                                                    <i class="fas fa-check"></i>
-                                                </x-elements.success-button>
-                                            </form>
-                                        @endif
-                                        <form class="ml-2" action="{{ route('manage-badges.delete', $badge) }}"
-                                            method="POST" onSubmit='confirmDelete("confirmBadgeDelete");'
-                                            onSubmit="return confirm('{{ __('Are you sure you want to delete this badge?') }}');">
-                                            @method('DELETE')
+                        @foreach ($badges as $badge)
+                            <tr>
+                                <td class="d-flex overflow-hidden" style="height: 50px;">
+                                    <img class="avatar"
+                                         src="{{ setting('avatar_imager') }}{{ $badge->user?->look }}&direction=2&headonly=1&head_direction=2&gesture=sml"
+                                         alt="">
+                                    {{ $badge->user?->username }}
+                                </td>
+                                <td>
+                                    <img
+                                        src="https://www.rattohotel.com/swf/c_images/Album1584/{{ $badge->badge_imaging }}"
+                                        height="32" width="32"/>
+                                    {{ $badge->badge_imaging }}
+                                </td>
+                                <td>{{ __('badge_status.' . $badge->status) }}</td>
+                                <td>
+                                    @if ($badge->status === 'open')
+                                        <form class="ml-2" action="{{ route('manage-badges.accept', $badge) }}"
+                                              method="POST" onSubmit='confirmDelete("confirmBadgeAccept");'
+                                              onSubmit="return confirm('{{ __('Are you sure you want to accept this badge?') }}');">
+                                            @method('PUT')
                                             @csrf
 
-                                            <x-elements.danger-button tooltip-text="{{ __('Delete badge') }}">
-                                                <i class="fas fa-trash"></i>
-                                            </x-elements.danger-button>
+                                            <x-elements.success-button tooltip-text="{{ __('Approve badge') }}">
+                                                <i class="fas fa-check"></i>
+                                            </x-elements.success-button>
                                         </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                    @endif
+                                    <form class="ml-2" action="{{ route('manage-badges.delete', $badge) }}"
+                                          method="POST" onSubmit='confirmDelete("confirmBadgeDelete");'
+                                          onSubmit="return confirm('{{ __('Are you sure you want to delete this badge?') }}');">
+                                        @method('DELETE')
+                                        @csrf
+
+                                        <x-elements.danger-button tooltip-text="{{ __('Delete badge') }}">
+                                            <i class="fas fa-trash"></i>
+                                        </x-elements.danger-button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
