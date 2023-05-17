1a28c304d366941a33f461a667fb5fbd2c6e0a20
Jean Nunes <jeanrnunes@gmail.com>
2023-05-17 16:04:42 -0300

    external text

--- resources/views/external-texts/index.blade.php
+++ resources/views/external-texts/index.blade.php
@@ -4,6 +4,43 @@
     <div class="container-fluid">
         <h3 class="text-dark mb-4">{{ __('Manage external texts') }}</h3>
 
+        <div class="row mb-4">
+            <div class="ml-2" data-toggle="tooltip" data-placement="top" title="You can search for multiple instances of each search criteria by comma seperating them. Eg. if you select page IDs you can enter the following 1,2,3,4,5">
+                <i class="far fa-question-circle"></i>
+            </div>
+
+            <div class="input-group d-flex justify-content-between">
+                <form action="{{ route('external-texts.index') }}" method="GET">
+                    <div class="d-block d-md-flex">
+                        <div class="col-12 col-lg-4">
+                            <select class="form-control" name="sort_by">
+                                <option value="page_ids">{{__('Key')}}</option>
+                                <option value="parent_ids">{{__('Value')}}</option>
+                            </select>
+                        </div>
+
+                        <div class="input-group col-12 col-lg-10">
+                            <div class="form-outline">
+                                <input style="width: 300px;" type="search" name="criteria" placeholder="Enter your search criteria" class="form-control">
+                            </div>
+
+                            <button type="submit" class="ml-2 btn btn-primary">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+                <form action="{{ route('catalog-pages.rcon-update') }}" method="POST" class="mr-2">
+                    @csrf
+
+                    <x-elements.danger-button>
+                        {{ __('Update catalog (RCON)') }}
+                    </x-elements.danger-button>
+                </form>
+            </div>
+        </div>
+
         <x-messages.flash-messages/>
 
         <div class="card shadow">
