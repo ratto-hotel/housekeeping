a70712fd2ce22c5d8de2a287251e98c783485277
Jean Nunes <jeanrnunes@gmail.com>
2023-05-17 17:00:09 -0300

    external text

--- resources/views/manage-badge/index.blade.php
+++ resources/views/manage-badge/index.blade.php
@@ -51,10 +51,12 @@
                                     {{ $badge->user?->username }}
                                 </td>
                                 <td>
-                                    <img
-                                        src="https://www.rattohotel.com/swf/c_images/Album1584/{{ $badge->badge_imaging }}"
-                                        height="32" width="32"/>
-                                    {{ $badge->badge_imaging }}
+                                    <div class="d-flex">
+                                        <img
+                                            src="https://www.rattohotel.com/swf/c_images/Album1584/{{ $badge->badge_imaging }}"
+                                            height="32" width="32"/>
+                                        <x-form.input disabled="1" value="{{ str_replace($badge->badge_imaging, '.gif', '') }}"/>
+                                    </div>
                                 </td>
                                 <td>{{ __('badge_status.' . $badge->status) }}</td>
                                 <td>
