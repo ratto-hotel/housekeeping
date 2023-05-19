7610814f2711aea4cc2f9b528ddde588087fcc50
Jean Nunes <jeanrnunes@gmail.com>
2023-05-19 13:57:24 -0300

    external text

--- app/Http/Controllers/ExternalTextsController.php
+++ app/Http/Controllers/ExternalTextsController.php
@@ -18,7 +18,7 @@ class ExternalTextsController extends Controller
 
         return view('external-texts.index', [
             'texts' => $texts
-                ->orderByAsc('key')
+                ->orderBy('key', 'ASC')
                 ->paginate(15)
         ]);
     }
